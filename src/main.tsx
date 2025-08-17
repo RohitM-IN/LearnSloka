e3a205e3034556e5b3d4d30b8a8c0084be02e77f
RohitM-IN <rohit.mahajan.in@gmail.com>
2025-08-17 17:51:14 +0530

    remove service worker

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>,
 )
-
-serviceWorker.register();
+serviceWorker.unregister();
+// serviceWorker.register();
 
